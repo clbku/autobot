40aa9b41a3034e24ac7a4284fadf3742ec695f74
Ly Hoang <lyhc@gisx.vn>
2023-04-09 09:51:08 +0700

    build: v0.0.0

--- .autobot/git.sh
+++ .autobot/git.sh
@@ -4,6 +4,8 @@ branch_name=$(git symbolic-ref --short HEAD)
 retcode=$?
 non_push_suffix="_local"
 
+NEW_TAG="v0.0.1"
+
 # Only push if branch_name was found (my be empty if in detached head state)
 if [ $retcode -eq 0 ] ; then
     #Only push if branch_name does not end with the non-push suffix
@@ -13,6 +15,9 @@ if [ $retcode -eq 0 ] ; then
         echo
         git add -A;
         git commit -m "build: v0.0.0";
+        git tag $NEW_TAG
+        echo "Tagged with $NEW_TAG"
+        git push --tags
         echo
         echo "**** Pushing current branch $branch_name to origin [i4h post-commit hook]"
         echo
